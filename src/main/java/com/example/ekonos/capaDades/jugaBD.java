7e7a44100e947bae2dda9cd196f03c5aa1df7b3a
a20pedlopcor <a20pedlopcor@inspedralbes.cat>
2022-03-25 11:57:24 +0100

    Estadistica a imprimir

--- src/main/java/com/example/ekonos/capaDades/jugaBD.java
+++ src/main/java/com/example/ekonos/capaDades/jugaBD.java
@@ -11,13 +11,28 @@ import java.util.ArrayList;
 public class jugaBD {
     public int idJugador;
     public int idPartida;
-    public jugaBD(int idJugador, int idPartida){
-        this.idJugador=idJugador;
-        this.idPartida=idPartida;
+    public int nAccionsAlpha;
+    public int nAccionsBeta;
+    public int nAccionsDelta;
+    public int nAccionsGamma;
+    public int nAccionsEpsilon;
+    public int nAccionsOmega;
+    public jugaBD(int idJugador, int idPartida,
+                  int nAccionsAlpha, int nAccionsBeta, int nAccionsDelta, int nAccionsGamma,
+                  int nAccionsEpsilon, int nAccionsOmega) {
+        this.idJugador = idJugador;
+        this.idPartida = idPartida;
+        this.nAccionsAlpha = nAccionsAlpha;
+        this.nAccionsBeta = nAccionsBeta;
+        this.nAccionsDelta = nAccionsDelta;
+        this.nAccionsGamma = nAccionsGamma;
+        this.nAccionsEpsilon = nAccionsEpsilon;
+        this.nAccionsOmega = nAccionsOmega;
     }
+
     public static ArrayList<jugaBD> obtenirJugades(Connection coneccio) throws SQLException {
         ArrayList <jugaBD> jugadesBD = new ArrayList<>();
-        String query = "select id_Jugador, id_Partida from JUGA";
+        String query = "select id_Jugador, id_Partida, nAccionsAlpha, nAccionsBeta, nAccionsDelta, nAccionsGamma, nAccionsEpsilon, nAccionsOmega from JUGA";
         try (Statement stmt = coneccio.createStatement()) {
             ResultSet rs = stmt.executeQuery(query);
             int i=0;
@@ -26,10 +41,22 @@ public class jugaBD {
                 String jugadorNom = rs.getString("nom");
                 int jugadorID = rs.getInt("id_Jugador");
                 int partidaID = rs.getInt("id_Partida");
-                jugadesBD.add(new jugaBD(jugadorID, partidaID));
+                int nAlpha = rs.getInt("nAccAlpha");
+                int nBeta = rs.getInt("nAccBeta");
+                int nDelta =rs.getInt("nAccDelta");
+                int nGamma =rs.getInt("nAccGamma");
+                int nEpsilon =rs.getInt("nAccEpsilon");
+                int nOmega = rs.getInt("nAccOmega");
+                jugadesBD.add(new jugaBD(jugadorID, partidaID, nAlpha, nBeta, nDelta, nGamma, nEpsilon, nOmega));
 
                 System.out.println("ID Jugador: " + jugadesBD.get(i));
                 System.out.println("ID Partida: " + jugadesBD.get(i));
+                System.out.println("Alpha: " + nAlpha);
+                System.out.println("Beta: " + nBeta);
+                System.out.println("Delta: " + nDelta);
+                System.out.println("Gamma: " + nGamma);
+                System.out.println("Epsilon: " + nEpsilon);
+                System.out.println("Omega: " + nOmega);
                 i++;
             }
         } catch (SQLException e) {
